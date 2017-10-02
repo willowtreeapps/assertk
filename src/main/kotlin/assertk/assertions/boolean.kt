3e286e6046af68f7e5c0e4c8427753d3ceaf83e7
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2017-10-02 15:41:20 -0400

    Added docs for boolean assertions.

--- src/main/kotlin/assertk/assertions/boolean.kt
+++ src/main/kotlin/assertk/assertions/boolean.kt
@@ -3,11 +3,19 @@ package assertk.assertions
 import assertk.Assert
 import assertk.assertions.support.expected
 
+/**
+ * Asserts the boolean is true.
+ * @see [isFalse]
+ */
 fun Assert<Boolean>.isTrue() {
     if (actual) return
     expected("to be true")
 }
 
+/**
+ * Asserts the boolean is false.
+ * @see [isTrue]
+ */
 fun Assert<Boolean>.isFalse() {
     if (!actual) return
     expected("to be false")
