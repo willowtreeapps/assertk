d25d5ce23399c21436ed69510ead7cd8d9a1c7c9
Dieter Bogdoll <dieter@bogdoll.net>
2017-10-08 12:58:08 +0200

    added assertions about the checksum of the stream content Direct support checksums are CRC32, MD5, SHA1, SHA256. There is also an assertion which takes an string for the algorithm, as used by java.security.MessageDigest.getInstance

--- src/main/kotlin/assertk/assertions/inputstream.kt
+++ src/main/kotlin/assertk/assertions/inputstream.kt
@@ -3,6 +3,8 @@ package assertk.assertions
 import assertk.Assert
 import assertk.assertions.support.expected
 import java.io.InputStream
+import java.security.MessageDigest
+import java.util.zip.CRC32
 
 /** Asserts that the actual stream has the same content as the other stream.
  * Both [InputStream]s will be closed by the assertion.
@@ -30,6 +32,79 @@ fun Assert<InputStream>.hasNotSameContentAs(other: InputStream) {
         expected("streams not to be equal, but they were equal")
     }
 
+}
+
+/** Asserts that compute the MD5 hash of the stream content.
+ *
+ * @param expectedChecksum the expected checksum, every byte is separated by ':'
+ */
+fun Assert<InputStream>.hasMD5(expectedChecksum: String) = hasChecksum("MD5", expectedChecksum)
+
+/** Asserts that compute the SHA-1 hash of the stream content.
+ *
+ * @param expectedChecksum the expected checksum, every byte is separated by ':'
+ */
+fun Assert<InputStream>.hasSHA1(expectedChecksum: String) = hasChecksum("SHA-1", expectedChecksum)
+
+/** Asserts that compute the SHA-256 hash of the stream content.
+ *
+ * @param expectedChecksum the expected checksum, every byte is separated by ':'
+ */
+fun Assert<InputStream>.hasSHA256(expectedChecksum: String) = hasChecksum("SHA-256", expectedChecksum)
+
+/** Asserts that compute the CRC32 hash of the stream content.
+ *
+ * @param expectedChecksum the expected checksum
+ */
+fun Assert<InputStream>.hasCRC32(expectedChecksum: Long) {
+    actual.use {
+        val crc32 = CRC32()
+        val buffer = ByteArray(BUFFER_SIZE)
+
+        while (true) {
+            val len = actual.read(buffer)
+            if (len < 0) break
+            crc32.update(buffer, 0, len)
+        }
+
+        if (crc32.value != expectedChecksum) {
+            expected("checksum to be $expectedChecksum, but it was ${crc32.value}")
+        }
+    }
+}
+
+/** Asserts that compute an user specified hash of the stream content.
+ *
+ * @param algorithm as used by the [MessageDigest.getAlgorithm]
+ * @param expectedChecksum the expected expectedChecksum
+ *
+ */
+fun Assert<InputStream>.hasChecksum(algorithm: String, expectedChecksum: String) {
+    val actualChecksum = actual.computeChecksum(algorithm)
+    if (expectedChecksum != actualChecksum) {
+        expected("checksum to be $expectedChecksum, but was $actualChecksum")
+    }
+}
+
+private fun InputStream.computeChecksum(algorithm: String): String {
+    use {
+        val buffer = ByteArray(BUFFER_SIZE)
+
+        val md = MessageDigest.getInstance(algorithm)
+        while (true) {
+            val len = read(buffer)
+            if (len < 0) break
+            md.update(buffer, 0, len)
+        }
+
+        return md.digest().toHexString()
+    }
+}
+
+private fun ByteArray.toHexString(separator: CharSequence = ":"): String {
+    val sb = StringBuilder()
+    this.asSequence().map { it.toHexString() }.joinTo(sb, separator)
+    return sb.toString()
 }
 
 private fun Byte.toHexString() = String.format("%02X", this)
@@ -145,7 +220,7 @@ private fun doTheStreamHaveTheSameContent(actual: InputStream, other: InputStrea
             // the following throw should be unnecessary, as the only way to leave the while loop is either
             // - somewhere in the while loop an exception is thrown
             // - somwhere in the while loop the method is left by a return statement
-             throw IllegalStateException("unreachable code")
+            throw IllegalStateException("unreachable code")
         }
     }
 }
--- src/test/kotlin/test/assertk/assertions/InputStreamSpec.kt
+++ src/test/kotlin/test/assertk/assertions/InputStreamSpec.kt
@@ -1,8 +1,7 @@
 package test.assertk.assertions
 
 import assertk.assert
-import assertk.assertions.hasNotSameContentAs
-import assertk.assertions.hasSameContentAs
+import assertk.assertions.*
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
@@ -101,6 +100,73 @@ class InputStreamSpec : Spek({
         it("streamB has not the same content as streamC (using hasNotSameContentAs)") {
             assert(streamB()).hasNotSameContentAs(streamC())
         }
+    }
+
+    given("a non empty stream where we compute the checksum") {
+
+        it("can compute CRC32 checksum") {
+            assert(streamB()).hasCRC32(4012917027L)
+        }
+
+        it("can compute MD5 checksum") {
+            assert(streamB()).hasMD5("7F:03:21:9A:CF:8D:68:49:03:2A:1B:B6:E4:F1:4C:3C")
+        }
+
+        it("can compute SHA-1 checksum") {
+            assert(streamB()).hasSHA1("5B:F8:75:11:92:91:EC:68:95:7E:D5:88:AD:ED:62:77:0E:44:AB:AF")
+        }
+
+        it("can compute SHA-256 checksum") {
+            assert(streamB()).hasSHA256("4B:7B:EC:25:AE:64:52:9C:76:0B:C3:E8:82:9F:03:79:D9:2A:91:6B:22:A9:3B:82:CA:B9:18:54:55:1A:AD:C0")
+        }
+
+    }
+
+    given("checksum of an empty stream") {
+
+        it("has CRC32 checksum") {
+            assert(emptyStream()).hasCRC32(0)
+        }
+
+        it("has MD5 checksum") {
+            assert(emptyStream()).hasMD5("D4:1D:8C:D9:8F:00:B2:04:E9:80:09:98:EC:F8:42:7E")
+        }
+
+        it("has SHA-1 checksum") {
+            assert(emptyStream()).hasSHA1("DA:39:A3:EE:5E:6B:4B:0D:32:55:BF:EF:95:60:18:90:AF:D8:07:09")
+        }
+
+        it("has SHA-256 checksum") {
+            assert(emptyStream()).hasSHA256("E3:B0:C4:42:98:FC:1C:14:9A:FB:F4:C8:99:6F:B9:24:27:AE:41:E4:64:9B:93:4C:A4:95:99:1B:78:52:B8:55")
+        }
+    }
+
+    given("a non empty stream where we compute the checksum wrongly") {
+
+        it("can compute CRC32 checksum") {
+            assertThatThrownBy {
+                assert(streamB()).hasCRC32(-1)
+            }.hasMessage("expected checksum to be -1, but it was 4012917027")
+        }
+
+        it("can compute MD5 checksum") {
+            assertThatThrownBy {
+                assert(streamB()).hasMD5("WRONG_CHECKSUM")
+            }.hasMessage("expected checksum to be WRONG_CHECKSUM, but was 7F:03:21:9A:CF:8D:68:49:03:2A:1B:B6:E4:F1:4C:3C")
+        }
+
+        it("can compute SHA-1 checksum") {
+            assertThatThrownBy {
+                assert(streamB()).hasSHA1("WRONG_CHECKSUM")
+            }.hasMessage("expected checksum to be WRONG_CHECKSUM, but was 5B:F8:75:11:92:91:EC:68:95:7E:D5:88:AD:ED:62:77:0E:44:AB:AF")
+        }
+
+        it("can compute SHA-256 checksum") {
+            assertThatThrownBy {
+                assert(streamB()).hasSHA256("WRONG_CHECKSUM")
+            }.hasMessage("expected checksum to be WRONG_CHECKSUM, but was 4B:7B:EC:25:AE:64:52:9C:76:0B:C3:E8:82:9F:03:79:D9:2A:91:6B:22:A9:3B:82:CA:B9:18:54:55:1A:AD:C0")
+        }
+
     }
 })
 
