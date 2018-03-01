05f6b7ec12dfdc3d7dd31c6792b13e9a781a6f0d
Eric Vantillard <eric.vantillard@evaxion.fr>
2018-03-01 09:18:52 +0100

    Fix broken test for #58

--- src/test/java/test/assertk/assertions/JavaNullableString.java
+++ src/test/java/test/assertk/assertions/JavaNullableString.java
@@ -1,8 +1,19 @@
 package test.assertk.assertions;
 
+import javax.annotation.Nullable;
+
 public class JavaNullableString {
 
     public static String string() {
         return null;
+    }
+
+    @Nullable
+    public static String stringWithNullableAnnotation() {
+        return null;
+    }
+
+    public static String nonNullableString() {
+        return "nonNullableString";
     }
 }
