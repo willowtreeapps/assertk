2b10aabf922a2911d82da23dc398de46814ef220
Evan Tatarka <evan@tatarka.me>
2018-08-04 12:25:14 -0400

    Update README and CHANGELOG to prepare for release 0.11

--- assertk-js/src/main/kotlin/assertk/assertions/support/support.kt
+++ assertk-js/src/main/kotlin/assertk/assertions/support/support.kt
@@ -1,7 +1,5 @@
 package assertk.assertions.support
 
 internal actual fun displayPlatformSpecific(value: Any?): String {
-    "".toList().joinToString(transform = { it.toByte().toString()})
     return value.toString()
-
 }
