a84fe525c310a97e6970d7e3a1095e171a2f6ea4
Martin Treurnicht <martin@treurnicht.io>
2018-08-30 13:59:48 -0700

    Cleanup number compare

--- assertk-common/src/main/kotlin/assertk/assertions/number.kt
+++ assertk-common/src/main/kotlin/assertk/assertions/number.kt
@@ -9,7 +9,7 @@ import assertk.assertions.support.show
  * @see [isNotZero]
  */
 fun <T : Number> Assert<T>.isZero() {
-    if (actual == 0) return
+    if (actual.toDouble() == 0.0) return
     expected("to be 0 but was:${show(actual)}")
 }
 
@@ -18,7 +18,7 @@ fun <T : Number> Assert<T>.isZero() {
  * @see [isZero]
  */
 fun <T : Number> Assert<T>.isNotZero() {
-    if (actual != 0) return
+    if (actual.toDouble() != 0.0) return
     expected("to not be 0")
 }
 
@@ -26,9 +26,8 @@ fun <T : Number> Assert<T>.isNotZero() {
  * Asserts the number is greater than 0.
  * @see [isNegative]
  */
-fun <T> Assert<T>.isPositive() where T : Number, T : Comparable<T> {
-    @Suppress("UNCHECKED_CAST", "UnsafeCast")
-    if (actual > 0 as T) return
+fun <T : Number> Assert<T>.isPositive() {
+    if (actual.toDouble() > 0) return
     expected("to be positive but was:${show(actual)}")
 }
 
@@ -36,8 +35,7 @@ fun <T> Assert<T>.isPositive() where T : Number, T : Comparable<T> {
  * Asserts the number is less than 0.
  * @see [isPositive]
  */
-fun <T> Assert<T>.isNegative() where T : Number, T : Comparable<T> {
-    @Suppress("UNCHECKED_CAST", "UnsafeCast")
-    if (actual < 0 as T) return
+fun <T : Number> Assert<T>.isNegative() {
+    if (actual.toDouble() < 0) return
     expected("to be negative but was:${show(actual)}")
 }
--- assertk-jvm/src/test/kotlin/test/assertk/assertions/support/NumberSpec.kt
+++ assertk-jvm/src/test/kotlin/test/assertk/assertions/support/NumberSpec.kt
@@ -1,5 +1,7 @@
 package test.assertk.assertions.support
 
+import assertk.assertions.isNegative
+import assertk.assertions.isPositive
 import assertk.assertions.isZero
 import org.junit.Test
 import java.math.BigDecimal
@@ -37,5 +39,51 @@ class BigIntegerSpec_a_Number_On_isZero() {
             assertk.assert(BigInteger.ONE).isZero()
         }
         assertEquals("expected to be 0 but was:<1>", error.message)
+    }
+}
+
+class BigDecimalSpec_a_Number_On_isPositive() {
+    @Test
+    fun it_Given_a_positive_number_test_should_pass() {
+        assertk.assert(BigDecimal.ONE).isPositive()
+    }
+
+    @Test
+    fun it_Given_a_zero_number_test_should_fail() {
+        val error = assertFails {
+            assertk.assert(BigDecimal.ZERO).isPositive()
+        }
+        assertEquals("expected to be positive but was:<0>", error.message)
+    }
+
+    @Test
+    fun it_Given_a_negative_number_test_should_fail() {
+        val error = assertFails {
+            assertk.assert(BigDecimal.ONE.negate()).isPositive()
+        }
+        assertEquals("expected to be positive but was:<-1>", error.message)
+    }
+}
+
+class NumberSpec_a_Number_On_isNegative() {
+    @Test
+    fun it_Given_a_zero_number_test_should_fail() {
+        val error = assertFails {
+            assertk.assert(BigDecimal.ZERO).isNegative()
+        }
+        assertEquals("expected to be negative but was:<0>", error.message)
+    }
+
+    @Test
+    fun it_Give_a_positive_number_test_should_fail() {
+        val error = assertFails {
+            assertk.assert(BigDecimal.ONE).isNegative()
+        }
+        assertEquals("expected to be negative but was:<1>", error.message)
+    }
+
+    @Test
+    fun it_Given_a_negative_number_test_should_pass() {
+        assertk.assert(BigDecimal.ONE.negate()).isNegative()
     }
 }
