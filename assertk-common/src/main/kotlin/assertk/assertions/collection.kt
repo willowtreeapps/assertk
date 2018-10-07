6b293a5f5aae0a2298c39a7f184979c7e65ec98e
osamarao <osama.rao@willowtreeapps.com>
2018-10-07 15:56:50 -0400

    Added failure when list has less than expected elements

--- assertk-common/src/main/kotlin/assertk/assertions/collection.kt
+++ assertk-common/src/main/kotlin/assertk/assertions/collection.kt
@@ -90,9 +90,13 @@ fun <T : Collection<*>> Assert<T>.containsAll(vararg elements: Any?) {
  * @see [containsAll]
  */
 fun <T : Collection<*>> Assert<T>.containsOnly(vararg elements: Any?) {
-    containsAll(*elements)
-    val unexpected = actual.filterNot { elements.contains(it) }
-    if (unexpected.isEmpty())
+    val notInActual = elements.filterNot { it in actual }
+    val notInExpected = actual.filterNot { elements.contains(it) }
+    if (notInExpected.isEmpty() && notInActual.isEmpty())
         return
-    expected("to contain only:${show(elements)} but extra elements were found:${show(unexpected)}")
+    if (notInActual.isNotEmpty()){
+        expected("to contain only:${show(elements)} but some elements were not found:${show(notInActual)}")
+    } else if (notInExpected.isNotEmpty()){
+        expected("to contain only:${show(elements)} but extra elements were found:${show(notInExpected)}")
+    }
 }
--- assertk-common/src/test/kotlin/test/assertk/assertions/CollectionTest.kt
+++ assertk-common/src/test/kotlin/test/assertk/assertions/CollectionTest.kt
@@ -113,5 +113,13 @@ class CollectionTest {
         }
         assertEquals("expected to contain only:<[2, 1]> but extra elements were found:<[3]>", error.message)
     }
+
+    @Test fun containsOnly_less_elements_fails() {
+        val error = assertFails{
+            assert(listOf(1, 2, 3)).containsOnly(2, 1, 3, 4)
+        }
+        assertEquals("expected to contain only:<[2, 1, 3, 4]> but some elements were not found:<[4]>", error.message)
+    }
+
     //endregion
 }
