a4af82ca019f279e79e2fa1923e457313f64c2b3
Evan Tatarka <evan@tatarka.me>
2019-06-04 23:07:24 -0400

    Inline catch for better use with coroutines

--- src/jsTest/kotlin/test/assertk/JSAssertBlockTest.kt
+++ src/jsTest/kotlin/test/assertk/JSAssertBlockTest.kt
@@ -3,9 +3,10 @@ package test.assertk
 import assertk.assertThat
 import assertk.assertions.hasMessage
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
 import assertk.assertions.isPositive
 import assertk.assertions.support.show
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import assertk.catch
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 import kotlin.test.Test
@@ -36,18 +37,23 @@ class JSAssertBlockTest {
         )
     }
 
-    @UseExperimental(ExperimentalCoroutinesApi::class)
     @Test fun returnedValue_works_in_coroutine_test() = GlobalScope.promise {
         assertThat {
             asyncReturnValue()
         }.returnedValue { isEqualTo(1) }
     }
 
-    @UseExperimental(ExperimentalCoroutinesApi::class)
     @Test fun returnedValue_exception_works_in_coroutine_test() = GlobalScope.promise {
         assertThat {
             asyncThrows()
         }.thrownError { hasMessage("test") }
+    }
+
+    @Test fun catch_works_in_corountine_test() = GlobalScope.promise {
+        val error = catch {
+            asyncThrows()
+        }
+        assertThat(error).isNotNull().hasMessage("test")
     }
 
     @Suppress("RedundantSuspendModifier")
