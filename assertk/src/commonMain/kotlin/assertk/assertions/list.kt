8c22512a8e1c0d7eb307f39884be8b3f0631b47e
Flávio Siqueira Prado <flavio.s.prado@gmail.com>
2021-08-20 07:08:18 +0300

    Remove index function from list
    Remove index function from list because we can make it generic with collections.

--- assertk/src/commonMain/kotlin/assertk/assertions/list.kt
+++ assertk/src/commonMain/kotlin/assertk/assertions/list.kt
@@ -7,22 +7,6 @@ import assertk.assertions.support.expectedListDiff
 import assertk.assertions.support.show
 
 /**
- * Returns an assert that assertion on the value at the given index in the list.
- *
- * ```
- * assertThat(listOf(0, 1, 2)).index(1).isPositive()
- * ```
- */
-fun <T> Assert<List<T>>.index(index: Int): Assert<T> =
-    transform(appendName(show(index, "[]"))) { actual ->
-        if (index in actual.indices) {
-            actual[index]
-        } else {
-            expected("index to be in range:[0-${actual.size}) but was:${show(index)}")
-        }
-    }
-
-/**
  * Asserts the list contains exactly the expected elements. They must be in the same order and
  * there must not be any extra elements.
  *
