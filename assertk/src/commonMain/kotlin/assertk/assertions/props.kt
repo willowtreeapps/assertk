31bcd4d83b34944151fc1e6398033f01bc47280a
mbjelac <marko.bjelac@gmail.com>
2021-09-08 23:50:46 +0200

    Add assertion for multiple object properties with readable specification syntax

---
+++ assertk/src/commonMain/kotlin/assertk/assertions/props.kt
@@ -0,0 +1,27 @@
+package assertk.assertions
+
+import assertk.Assert
+
+/**
+ * Asserts equality of the object's specified props.
+ *
+ * Use [Pair.to] infix constructor to achieve better readability:
+ *
+ * ```
+ * assertThat(MyObject("a", 1, true))
+ *   .propsEqualTo(
+ *     MyObject::name to "a",
+ *     MyObject::amount to 1,
+ *     MyObject::flag to true
+ *   )
+ * ```
+ *
+ * @see[Pair.to]
+ */
+fun <T> Assert<T>.propsEqualTo(vararg propEqualities: Pair<(T) -> Any, Any>) =
+    given { actual ->
+        assertThat(propEqualities.map { it.first(actual) })
+            .isEqualTo(
+                propEqualities.map { it.second }
+            )
+    }
---
+++ assertk/src/commonTest/kotlin/test/assertk/assertions/PropsTest.kt
@@ -0,0 +1,44 @@
+package test.assertk.assertions
+
+import assertk.assertThat
+import assertk.assertions.isFailure
+import assertk.assertions.isInstanceOf
+import assertk.assertions.propsEqualTo
+import com.willowtreeapps.opentest4k.AssertionFailedError
+import kotlin.test.Test
+
+class PropsTest {
+
+    data class MyObject(
+        val name: String,
+        val amount: Number,
+        val flag: Boolean
+    )
+
+    private val anObject = MyObject("a", 1, true)
+
+    @Test
+    fun propsEqualTo_equalValues_passes() {
+
+        assertThat(anObject)
+            .propsEqualTo(
+                MyObject::name to "a",
+                MyObject::amount to 1,
+                MyObject::flag to true
+            )
+    }
+
+    @Test
+    fun propsEqualTo_oneValueDifferent_fails() {
+
+        assertThat {
+            assertThat(anObject).propsEqualTo(
+                MyObject::name to "a",
+                MyObject::amount to 2,
+                MyObject::flag to true
+            )
+        }
+            .isFailure()
+            .isInstanceOf(AssertionFailedError::class)
+    }
+}
