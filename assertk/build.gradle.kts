80629c4d670c7417a997e06adedfc816693b2e84
Jake Wharton <jw@squareup.com>
2023-03-16 00:46:47 -0400

    Use runTest from the coroutine test library

--- assertk/build.gradle.kts
+++ assertk/build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
-                implementation(libs.kotlin.coroutines)
+                implementation(libs.kotlinx.coroutines)
             }
             kotlin.srcDir(compileTestTemplates)
         }
