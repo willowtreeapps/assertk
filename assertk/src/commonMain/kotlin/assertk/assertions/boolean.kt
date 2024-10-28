3ef715cfd8e55813324777b54111062688740b7f
Jake Wharton <jw@squareup.com>
2024-10-28 10:48:04 -0400

    Widen boolean asserts to operate on nullable types
    This behavior brings it more line with how '==' behaves in the language.

--- assertk/src/commonMain/kotlin/assertk/assertions/boolean.kt
+++ assertk/src/commonMain/kotlin/assertk/assertions/boolean.kt
@@ -2,21 +2,22 @@ package assertk.assertions
 
 import assertk.Assert
 import assertk.assertions.support.expected
+import assertk.assertions.support.fail
 
 /**
  * Asserts the boolean is true.
  * @see [isFalse]
  */
-fun Assert<Boolean>.isTrue() = given { actual ->
-    if (actual) return
-    expected("to be true")
+fun Assert<Boolean?>.isTrue() = given { actual ->
+    if (actual == true) return
+    fail(true, actual)
 }
 
 /**
  * Asserts the boolean is false.
  * @see [isTrue]
  */
-fun Assert<Boolean>.isFalse() = given { actual ->
-    if (!actual) return
-    expected("to be false")
+fun Assert<Boolean?>.isFalse() = given { actual ->
+    if (actual == false) return
+    fail(false, actual)
 }
